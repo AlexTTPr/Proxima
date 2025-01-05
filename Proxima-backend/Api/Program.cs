239cd85bc34649cc1977ea19180001f008007a7e
Александр Абрамов <alemov27122011@gmail.com>
2025-01-05 18:56:43 +0300

    add ScalarApi and remove default files

--- Proxima-backend/Api/Program.cs
+++ Proxima-backend/Api/Program.cs
@@ -1,33 +1,29 @@
+using Scalar.AspNetCore;
 
-namespace Api;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+if(app.Environment.IsDevelopment())
 {
-	public static void Main(string[] args)
+	app.MapScalarApiReference(opt =>
 	{
-		var builder = WebApplication.CreateBuilder(args);
-
-		// Add services to the container.
-
-		builder.Services.AddControllers();
-		// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-		builder.Services.AddOpenApi();
-
-		var app = builder.Build();
-
-		// Configure the HTTP request pipeline.
-		if(app.Environment.IsDevelopment())
-		{
-			app.MapOpenApi();
-		}
-
-		app.UseHttpsRedirection();
-
-		app.UseAuthorization();
-
-
-		app.MapControllers();
-
-		app.Run();
-	}
+		opt
+			.WithTitle("Proxima API")
+			.WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.Axios)
+			.WithTheme(ScalarTheme.Alternate);
+	});
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
